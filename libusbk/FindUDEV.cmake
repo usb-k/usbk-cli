12dc95f99e857331b0fcfa51501595202bafe237
Timuçin Anuşlu <timucin@usb-k.com>
2011-04-14 15:49:28 +0300

    CMakeList.txt files are edited and arranged.

---
+++ libusbk/FindUDEV.cmake
@@ -0,0 +1,34 @@
+#
+# Set global variable UDEV to TRUE if both udev lib and include files are found
+#
+
+
+# Look for FUSE library
+FIND_LIBRARY(UDEV_LIB
+             NAMES udev
+             PATHS /lib
+                   /usr/lib
+                   /usr/local/lib)
+
+
+IF(NOT UDEV_LIB)
+  MESSAGE(STATUS "Unable to find 'udev' library")
+  SET(UDEV FALSE)
+ELSE(NOT UDEV_LIB)
+  SET(UDEV TRUE)
+ENDIF(NOT UDEV_LIB)
+
+
+# Look for FUSE include files
+FIND_PATH(UDEV_INC
+          NAMES libudev.h
+          PATHS /usr/include
+                /usr/local/include)
+
+IF (NOT UDEV_INC)
+  MESSAGE(STATUS "Unable to find 'udev' include files")
+  SET(UDEV FALSE)
+ELSE(NOT UDEV_INC)
+  SET(UDEV TRUE)
+ENDIF(NOT UDEV_INC)
+
